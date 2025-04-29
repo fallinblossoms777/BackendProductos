27d30e16a1f87fa9b5a1e075d9e400b6e7501d4a
fallinblossoms777 <166082974+fallinblossoms777@users.noreply.github.com>
2025-04-29 18:49:12 -0300

    Cambio mensaje de consola

--- index.js
+++ index.js
@@ -37,6 +37,6 @@ sequelize.sync().then (result => {
 });
 
 app.listen(6500,() => {
-    console.log('Servidor corriendo en el puerto 6500');
+    console.log('Servidor corriendo en el puerto 3600.');
 });
 
